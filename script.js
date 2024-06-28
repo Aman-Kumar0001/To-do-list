f44a3d48a75e14bf8c001f6b632d6e047dc29a4e
Aman Kumar <aman011908@gmail.com>
2024-06-28 14:45:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
 document.getElementById("btn").addEventListener("click", () => {
     let input = document.getElementById("input").value;
+    if (input == "") {
+        return
+    }
 
-    const p = "<p>";
-    const delIcon = '</p><div><button><i class="fa-solid fa-trash fa-xl"></i></button></div>';
-    const taskNow = document.createElement("div");
-
-    const taskNowB = document.createElement("p");
-    taskNowB.innerHTML = input;
-    taskNow.append(taskNowB);
-
-    const taskNowC = document.createElement("div");
-
-    const taskNowD = document.createElement("button");
-
-    const taskNowE = document.createElement("i");
-    taskNowE.classList.add("fa-solid");
-    taskNowE.classList.add("fa-trash");
-    taskNowE.classList.add("fa-xl");
-
-    taskNowD.append(taskNowE);
-    taskNowD.addEventListener("click", function () { taskNow.remove(); });
-    taskNowC.append(taskNowD);
-    taskNow.append(taskNowC);
-
-    document.getElementById("parent").append(taskNow);
-})
-
-
-
-document.getElementById("todoListByTidyCoder").addEventListener("keypress", (event) => {
-
-    if (event.key == "Enter") {
-        let input = document.getElementById("input").value;
+    else {
 
         const p = "<p>";
         const delIcon = '</p><div><button><i class="fa-solid fa-trash fa-xl"></i></button></div>';
@@ -56,6 +29,46 @@ document.getElementById("todoListByTidyCoder").addEventListener("keypress", (eve
         taskNow.append(taskNowC);
 
         document.getElementById("parent").append(taskNow);
+    }
+})
+
+
+
+document.getElementById("todoListByTidyCoder").addEventListener("keypress", (event) => {
+    let input = document.getElementById("input").value;
+
+    if (input == "") {
+        return
+    }
+
+    else {
+
+        if (event.key == "Enter") {
+
+            const p = "<p>";
+            const delIcon = '</p><div><button><i class="fa-solid fa-trash fa-xl"></i></button></div>';
+            const taskNow = document.createElement("div");
+
+            const taskNowB = document.createElement("p");
+            taskNowB.innerHTML = input;
+            taskNow.append(taskNowB);
+
+            const taskNowC = document.createElement("div");
+
+            const taskNowD = document.createElement("button");
+
+            const taskNowE = document.createElement("i");
+            taskNowE.classList.add("fa-solid");
+            taskNowE.classList.add("fa-trash");
+            taskNowE.classList.add("fa-xl");
+
+            taskNowD.append(taskNowE);
+            taskNowD.addEventListener("click", function () { taskNow.remove(); });
+            taskNowC.append(taskNowD);
+            taskNow.append(taskNowC);
+
+            document.getElementById("parent").append(taskNow);
+        }
     }
 
 })
